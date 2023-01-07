abb217017dcd5bb0f18eb05816fa07f323a883bb
Mike Driver <mdriver@imaginecommunications.com>
2023-01-07 19:17:07 +0000

    s3 buckets changed

--- main.tf
+++ main.tf
@@ -12,19 +12,36 @@ variable secret-key{}
 
 #=======
 resource "aws_instance" "demo_linux_web_server" {
-ami = "ami-0e80a462ede03e653"
-instance_type = "t2.micro"
-tags = {
-Name = "demo_t2_micro_linux_web_server"
-  }
+    ami = "ami-0e80a462ede03e653"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "demo_t2_micro_linux_web_server"
+        Environment = "Dev"
+    }
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "mikes-demo-bucket988876r"
-  acl    = "private"
+resource "aws_s3_bucket" "this_b" {
+    bucket = "mikes-demo-bucket427"
+    tags = {
+        Name        = "Mikes bucket"
+        Environment = "Dev"
+    }
+}
 
-  tags = {
-    Name        = "Mikes bucket"
-    Environment = "Dev"
-  }
+resource "aws_s3_bucket_acl" "b_acl" {
+    bucket = aws_s3_bucket.this_b.id
+    acl    = "private"
+}
+
+resource "aws_s3_bucket" "this_b2" {
+    bucket = "mikes-demo-bucket4272"
+    tags = {
+        Name        = "Mikes bucket2"
+        Environment = "Dev"
+    }
+}
+
+resource "aws_s3_bucket_acl" "b2_acl" {
+    bucket = aws_s3_bucket.this_b2.id
+    acl    = "public-read"
 }
