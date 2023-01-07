7128944b56757c88ba4066c382ceccdad658b4b5
Mike Driver <mdriver@imaginecommunications.com>
2023-01-07 17:16:13 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+# A terraform project for demonstration purposes 
+provider "aws" {
+    region = "eu-west-2"
+    # these credentials are held in here: c:\Secrets\secret.tfvars for security reasons and need to be referenced on command line:
+    # terraform apply -var-file c:/secrets/secret.tfvars
+    # terraform destroy -var-file c:/secrets/secret.tfvars
+    access_key = var.access-key
+    secret_key = var.secret-key
+}
+variable access-key{}
+variable secret-key{}
+
+#=======
+resource "aws_instance" "demo_linux_web_server" {
+ami = "ami-0e80a462ede03e653"
+instance_type = "t2.micro"
+tags = {
+Name = "demo_t2_micro_linux_web_server"
+  }
+}
